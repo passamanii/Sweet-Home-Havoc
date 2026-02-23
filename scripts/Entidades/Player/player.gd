1f4aacf439f577a33911f8f420df7b4d219115d5
Luís Felipe Passamani Santos <luisfelipepsantos@gmail.com>
2026-02-23 15:50:09 -0300

    Regeneration
    Added a regeneration logic to restore health to the player with 5 seconds being away from combat.

--- scripts/Entidades/Player/player.gd
+++ scripts/Entidades/Player/player.gd
@@ -15,7 +15,7 @@ var facing: Vector2 = Vector2.ZERO
 
 var xp: int = 0
 var level: int = 1
-var health: int = 5 #Cada coração equivale à 10hp
+var health: int = 30 #Cada coração equivale à 10hp
 var max_health: int = 30
 var damage: float = 10
 var regen: int = 0
@@ -25,6 +25,7 @@ var armor: float = 0
 @onready var hitbox_area: Area2D = $HitboxArea
 @onready var hitbox_collision: CollisionShape2D = $HitboxArea/HitboxCollision
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var regeneration_timer: Timer = $Regeneration_Timer
 
 signal player_died
 
@@ -49,7 +50,6 @@ func _physics_process(delta: float) -> void:
 
 func _process(_delta: float) -> void:
 	define_stats()
-	regenerate()
 
 func define_spawn():
 	if (Player_Tracking.spawn_pos != Vector2.ZERO):
@@ -58,14 +58,14 @@ func define_spawn():
 		facing = Player_Tracking.spawn_facing
 	
 func define_stats():
-	if (Player_Stats.xp != 0):
+	if (Player_Stats.xp != xp):
 		xp = Player_Stats.xp
-	if (Player_Stats.level != 1):
+	if (Player_Stats.level != level):
 		level = Player_Stats.level
-	if (Player_Stats.health != 30):
+	if (Player_Stats.health != health):
 		health = Player_Stats.health
 		max_health = Player_Stats.max_health
-	if (Player_Stats.damage != 10):
+	if (Player_Stats.damage != damage):
 		damage = Player_Stats.damage
 	if (Player_Stats.speed != SPEED):
 		SPEED = Player_Stats.speed	
@@ -75,10 +75,12 @@ func define_stats():
 		regen = Player_Stats.regen
 		
 func regenerate():
-	if health != max_health:
-		await get_tree().create_timer(5).timeout
-		health += 1 + regen
-		print(health)
+	var health_to_regenerate = 5 + regen
+	if Player_Stats.health != Player_Stats.max_health:
+		if (health_to_regenerate + Player_Stats.health) > Player_Stats.max_health:
+			Player_Stats.health = Player_Stats.max_health
+		else:
+			Player_Stats.health += health_to_regenerate
 		
 func attack() -> void:
 	if (Input.is_action_just_pressed("attack")):
@@ -177,9 +179,10 @@ func dash() -> void:
 	
 func get_hit(enemy_damage: int, hit_position: Vector2) -> void:
 	var direction = (global_position - hit_position).normalized()
-	knockback_velocity = direction * knockback_force
-	
 	var resultant_damage = enemy_damage - floor(armor / 10)
+	
+	knockback_velocity = direction * knockback_force
+	regeneration_timer.start()
 	Player_Stats.health -= resultant_damage
 	if (Player_Stats.health <= 0):
 		die()
@@ -204,3 +207,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if (anim_name.contains('Attack')):
 		is_attacking = false
 		hitbox_collision.disabled = true
+
+
+func _on_regeneration_timer_timeout() -> void:
+	regenerate()
