be0fec4d11c08aa2939272e3682eefdccf8e235e
Luís Felipe Passamani Santos <luisfelipepsantos@gmail.com>
2026-02-24 12:09:23 -0300

    Removed Cached .import files

--- scripts/UI/xp_popup.gd
+++ scripts/UI/xp_popup.gd
@@ -1,14 +1,8 @@
 class_name XPScript extends Label
 
-signal finished
-
 func _ready() -> void:
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(self, 'scale', Vector2(2, 2), 0.2)
 	tween.tween_property(self, 'scale', Vector2(2, 2), 2)
 	await tween.finished
-	tween.finished.connect(_finished)
 	queue_free()
-
-func _finished():
-	emit_signal('finished')
