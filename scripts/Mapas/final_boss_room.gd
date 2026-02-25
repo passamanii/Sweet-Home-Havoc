691466668e1098effac4d43e7863dee3b18133f0
Vinny Daros Trindade <122296481+vinnydarostrindad@users.noreply.github.com>
2026-02-24 22:24:02 -0300

    finish boss first fase

--- scripts/Mapas/final_boss_room.gd
+++ scripts/Mapas/final_boss_room.gd
@@ -1,1 +1,6 @@
 extends Node2D
+
+@export var player: BasePlayer
+
+func _ready() -> void:
+	player.camera_2d.zoom = Vector2(0.45, 0.45)
--- scripts/UI/Boss/boss_health.gd
+++ scripts/UI/Boss/boss_health.gd
@@ -1,1 +1,12 @@
 extends CanvasLayer
+
+@export var boss: Faimisson1
+@export var bar: TextureProgressBar
+
+func _ready() -> void:
+	bar.max_value = boss.max_health
+	bar.value = boss.health
+	boss.updated_health.connect(_on_updated_health)
+
+func _on_updated_health() -> void:
+	bar.value = boss.health
